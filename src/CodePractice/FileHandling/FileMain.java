70bdf8f2e95bbbc74145c55398785a9fda78d90c
sachin <handesachin15071993@gmail.com>
2024-01-14 13:00:58 +0530

    Deleted some unwanted files.

--- src/CodePractice/FileHandling/FileMain.java
+++ src/CodePractice/FileHandling/FileMain.java
@@ -2,7 +2,7 @@ package CodePractice.FileHandling;
 
 import java.io.*;
 
-public class FilePrograms {
+public class FileMain {
 		public static void main(String[] args){
 			FileOperations fileOperations=new FileOperations();
 //			fileOperations.creatFile(new File("demo.txt"));
